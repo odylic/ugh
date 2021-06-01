a79f08568e966cdc25c59207a230d5a8eda93fe5
kerri <75552647+kerriannercrawford@users.noreply.github.com>
2021-06-01 10:48:23 -0400

    ugh

---
+++ src/Completed.js
@@ -0,0 +1,7 @@
+export default function Completed(props) {
+  return (
+    <div className='task'>
+      <h3>{props.value || 'Default Task Value'}</h3>
+    </div>
+  );
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Body from './Body';
+import React from 'react';
 
 function App() {
   return (
